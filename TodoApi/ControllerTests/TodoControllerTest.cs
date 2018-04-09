c21d6f5a8bdece9c29469223eeb96a540e57fd96
mollies <mollie@8thlight.com>
2018-04-09 09:38:00 +0100

    Make an api

---
+++ TodoApi/ControllerTests/TodoControllerTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace TodoApi.ControllerTests
+{
+    [TestFixture]
+    public class TodoControllerTest
+    {
+
+        [Test]
+        public void DoesSomething
+        {
+            Assert.IsTrue(false, "should be true");
+        }
+    }
+}
